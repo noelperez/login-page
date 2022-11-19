fa9bad0dc820e3092b2fe6703d580b698320a7fe
Noel Perez <noel999@gmail.com>
2022-11-19 12:01:38 -0400

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import MainContainer from "./components/main-container/MainContainer";
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+
+
+        <MainContainer />
+        
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ client/src/components/main-container/MainContainer.jsx
@@ -0,0 +1,22 @@
+import './MainContainer.scss';
+
+function MainContainer() {
+  return (
+    <div className='main-container'>
+      
+      <div className="left">
+        <div className="img-container">
+
+        </div>
+      </div>
+      <div className="right">
+        <div className="right-container">
+
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default MainContainer;
